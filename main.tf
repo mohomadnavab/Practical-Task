d320e6a84173fb735f35a7b642552daa71adc6b1
mohomadnavab <143999298+mohomadnavab@users.noreply.github.com>
2024-07-27 16:13:06 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,18 @@
+#Provider.tf
 
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.60.0"
+    }
+  }
+}
 provider "aws" {
   region = "ap-south-1" 
 }
 
+#main.tf
 
 resource "aws_vpc" "web_vpc" {
   cidr_block = "10.0.0.0/16"
